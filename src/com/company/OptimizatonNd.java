722fc40449f24ed7feb48df88eade114adf1bcf1
XSmile2008 <XSmile2008@gmail.com>
2015-10-20 12:41:03 +0300

    add 4 laba

--- src/com/company/OptimizatonNd.java
+++ src/com/company/OptimizatonNd.java
@@ -4,4 +4,16 @@ package com.company;
  * Created by Starikov on 20.10.15.
  */
 public class OptimizatonNd {
+
+    static double k;
+    static double fk;
+
+    private static double f(double x[]) {
+        return 10*x[0]*x[0] + 4*x[0]*x[1] + x[1]*x[1] - 2*x[0] + x[1];
+    }
+
+    public static void gradientDescent(double e) {
+        double[] x0 = {0, 0};
+
+    }
 }
