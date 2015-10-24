3eb8f63229e07bc3aefc1aa97dad7c01ee2b279d
XSmile2008 <XSmile2008@gmail.com>
2015-10-25 00:00:52 +0300

    add Gauss_Seidel mothod

--- src/com/company/OptimizatonNd.java
+++ src/com/company/OptimizatonNd.java
@@ -20,12 +20,9 @@ public class OptimizatonNd {
         return Math.sqrt(sum);
     }
 
-    public static double[][] gradientDescent(Function f, Function[] fs, double[] x0, double e) {
+    public static double[][] gradientDescent(Function f, Function[] fs, double[] x, double e) {
         k = 0;
-        double[] x = x0;//TODO: remove
-        double fx = f.calc(x0);
         double nx = e;//norm(x^(k+1) - x^k)
-
         while (true) {
             k++;
 
@@ -45,7 +42,8 @@ public class OptimizatonNd {
                         else doubles[j] = x[j] - h[j] * ug[j];
                     return f.calc(doubles);
                 };
-                h[i] = Optimization1d.parabolas(func, x[0], 1, e)[0];
+                //h[i] = Optimization1d.parabolas(func, x[0], 1, e)[0];//TODO: check x[0] for hi
+                h[i] = Optimization1d.parabolas(func, 0, 1, e)[0];
             }
 
             /*Function nyan0 = h0 -> f.calc(x[0] - h0[0] * ug[0], x[1] - h[1] * ug[1]);
@@ -58,10 +56,37 @@ public class OptimizatonNd {
             for (int i = 0; i < x.length; i++) delta[i] = x[i] - (x[i] - h[i] * ug[i]);
             nx = norm(delta);
 
-            for (int i = 0; i < x.length; i++) x[i] -= h[i] * ug[i];
+            for (int i = 0; i < x.length; i++) x[i] -= h[i] * ug[i];//TODO: can be moved to h minimization loop
             System.out.println("\nk = " + k + " -----------------------------------");
             System.out.println("f(x^k = )" + f.calc(x));
             System.out.println("norm(x^(k+1) - x^k) = " + nx);
+        }
+    }
+
+    public static void Gauss_Seidel(Function f, double[] x, double e) {
+        k = 0;
+        double fx = f.calc(x);
+        int[] sign = new int[x.length]; for (int i : sign) i = 1;
+        double nx = e;//norm(x^(k+1) - x^k)
+        double[] delta = new double[x.length];
+        while (nx >= e) {
+            for (int j = 0; j < x.length; j++) {
+                //TODO: add sign finding
+                final int fj = j;
+                Function func = hj -> {
+                    double[] doubles = new double[x.length];
+                    for (int l = 0; l < x.length; l++)
+                        if (fj == l) doubles[l] = x[l] + hj[0] * sign[l] * e;
+                        else doubles[l] = x[l];
+                    return f.calc(doubles);
+                };
+                double h = Optimization1d.parabolas(func, x[0], 1, e)[0];
+                double temp = x[j];
+                x[j] = x[j] + h * sign[j] * e;
+                fx = f.calc(x);
+                delta[j] = x[j] - temp;
+            }
+            nx = norm(delta);
         }
     }
 }
